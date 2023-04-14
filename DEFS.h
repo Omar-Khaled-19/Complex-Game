9581c0fefc0803aa1b8a3b59c973e94f3e863f48
amrmgdy02 <amrmagdy722@gmail.com>
2023-04-14 16:00:23 +0200

    output1

--- DEFS.h
+++ DEFS.h
@@ -18,7 +18,7 @@ enum ActionType // The actions supported (add more as needed)
 	DELETE_GAME_OBJECT,
 	SAVE_GRID,
 	OPEN_GRID,
-	SWITCH_TO_PLAY_MODE,	// Go to Play Mode
+	TO_PLAY_MODE,	// Go to Play Mode
 	EXIT,			// Exit Application
 	///TODO: Add more action types of Design Mode
 
