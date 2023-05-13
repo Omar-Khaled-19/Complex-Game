346601442a33aa68f2043c795545aa590012b1b0
kassem72 <mohamedkassem018@gmail.com>
2023-05-13 04:11:30 +0300

    AddSnakeAction remaining validations

--- AddSnakeAction.cpp
+++ AddSnakeAction.cpp
@@ -2,3 +2,62 @@
 #include "Input.h"
 #include "Output.h"
 #include "Snake.h"
+
+
+AddSnakeAction::AddSnakeAction(ApplicationManager* pApp) : Action(pApp)
+{
+	// Initializes the pManager pointer of Action with the passed pointer
+}
+
+AddSnakeAction::~AddSnakeAction()
+{
+}
+
+void AddSnakeAction::ReadActionParameters()
+{
+	// Get a Pointer to the Input / Output Interfaces
+	Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
+
+	// Read the startPos parameter
+	pOut->PrintMessage("New Ladder: Click on its Start Cell ...");
+	startPos = pIn->GetCellClicked();
+
+	// Read the endPos parameter
+	pOut->PrintMessage("New Ladder: Click on its End Cell ...");
+	endPos = pIn->GetCellClicked();
+
+
+
+	///TODO: Make the needed validations on the read parameters
+
+
+
+	// Clear messages
+	pOut->ClearStatusBar();
+}
+
+void AddSnakeAction::Execute()
+{
+	// The first line of any Action Execution is to read its parameter first 
+	// and hence initializes its data members
+	ReadActionParameters();
+
+	// Create a snake object with the parameters read from the user
+	Snake* pSnake = new Snake(startPos, endPos);
+
+	Grid* pGrid = pManager->GetGrid(); // We get a pointer to the Grid from the ApplicationManager
+
+	// Add the card object to the GameObject of its Cell:
+	bool added = pGrid->AddObjectToCell(pSnake);
+
+	// if the GameObject cannot be added
+	if (!added)
+	{
+		// Print an appropriate message
+		pGrid->PrintErrorMessage("Error: Cell already has an object ! Click to continue ...");
+	}
+	// Here, the snake is created and added to the GameObject of its Cell, so we finished executing the AddSnakeAction
+
+}
--- AddSnakeAction.h
+++ AddSnakeAction.h
@@ -1,7 +1,18 @@
 #pragma once
 #include "Action.h"
-class AddSnakeAction :
-    public Action
+class AddSnakeAction : public Action
 {
+	CellPosition startPos; 
+	CellPosition endPos;
+public:
+	
+	AddSnakeAction(ApplicationManager* pApp);       //the constructor
+
+	virtual void ReadActionParameters(); // Reads the action parameters (startPos, endPos)
+
+	virtual void Execute(); // Creates a new snake Object then Sets this  object to the GameObject Pointer of its Cell
+
+	virtual ~AddSnakeAction();       
+
 };
 
