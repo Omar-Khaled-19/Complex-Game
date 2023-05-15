964e11d77c39da289f62b87e560dd94510312dae
LIGHT1079 <129695129+LIGHT1079@users.noreply.github.com>
2023-05-15 16:24:51 +0300

    switch to play mode

--- SwitchToPlayModeAction.h
+++ SwitchToPlayModeAction.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "Action.h"
+#include"Grid.h"
 class SwitchToPlayModeAction :public Action
-{
+{public:
+    SwitchToPlayModeAction(ApplicationManager* pApp);
+
+    virtual void ReadActionParameters();
+
+    virtual void Execute();
 };
 
